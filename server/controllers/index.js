db77c8a0d6af72157606df5ade39b6ad1747f3db
Hack Reactor Students <accounts@hackreactor.com>
2015-02-27 21:35:11 -0800

    Add client files to current files

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,14 +5,24 @@ var bluebird = require('bluebird');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      res.writeHead(200, "Hello");
+      console.log('Consoled In Messages GET!');
+      res.end("ended");
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      console.log('Consoled In Messages POST!');
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      console.log('Consoled In Users GET!');
+    },
+    post: function (req, res) {
+      console.log('Consoled In Users POST!');
+    }
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,36 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER NOT NULL,
+  Text VARCHAR(140) NOT NULL,
+  CreatedTime DATETIME(6) NOT NULL,
+  id_Users INTEGER NOT NULL,
+  id_Rooms INTEGER NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INTEGER NOT NULL,
+  Name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
 
+DROP TABLE IF EXISTS rooms;
+CREATE TABLE rooms (
+  id INTEGER NOT NULL,
+  Name VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
 
-
+ALTER TABLE messages ADD FOREIGN KEY (id_Users) REFERENCES users (id);
+ALTER TABLE messages ADD FOREIGN KEY (id_Rooms) REFERENCES rooms (id);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
