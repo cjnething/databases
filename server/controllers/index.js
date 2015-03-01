2b5df26699a86082d3dcc412417b363c7898b275
Hack Reactor Students <accounts@hackreactor.com>
2015-02-28 17:33:14 -0800

    Add functionality to post and get requests

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,17 +1,55 @@
 var models = require('../models');
 var bluebird = require('bluebird');
 
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "application/JSON"
+};
+
+var statusCode = 200;
+
+
+var collectData = function(request, callback){
+    var data = "";
+    request.on('data', function(chunk){
+      data += chunk;
+    });
+    request.on('end', function(){
+      callback(JSON.parse(data));
+    });
+  };
+  var sendResponse = function(response, data, statusCode){
+    statusCode = statusCode || 200;
+    response.writeHead(statusCode, headers);
+    response.end(JSON.stringify(data));
+  };
+
+var sendBackMsg = [
+  // Useful for debugging
+  {
+    text: 'hello world',
+    username: 'fred'
+  }
+];
 
 
 module.exports = {
   messages: {
-    get: function (req, res) {
-      res.writeHead(200, "Hello");
-      console.log('Consoled In Messages GET!');
-      res.end("ended");
+    get: function (request, response) {
+      console.log("we made it");
+      sendResponse(response, {results: sendBackMsg});
     }, // a function which handles a get request for all messages
-    post: function (req, res) {
+    post: function (request, response) {
       console.log('Consoled In Messages POST!');
+      collectData(request, function(message) {
+        console.log("message");
+        sendBackMsg.push(message);
+        sendResponse(response, {username: 'fred'}, 201);
+      });
+
     } // a function which handles posting a message to the database
   },
 
@@ -19,9 +57,13 @@ module.exports = {
     // Ditto as above
     get: function (req, res) {
       console.log('Consoled In Users GET!');
+      //res.writeHead(200, headers);
+      //res.end(headers);
     },
     post: function (req, res) {
       console.log('Consoled In Users POST!');
+      //res.writeHead(200, headers);
+      //res.end(headers);
     }
   }
 };
--- server/models/index.js
+++ server/models/index.js
@@ -1,18 +1,40 @@
 var db = require('../db');
 
-
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "application/JSON"
+};
 
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (req, res) {
+      console.log("getmodels");
+      //res.writeHead(200, headers);
+      //res.end(headers);
+    }, // a function which produces all the messages
+    post: function (req, res) {
+      console.log("post models");
+      //res.writeHead(200, headers);
+      //res.end(headers);
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (req, res) {
+      console.log("get users models");
+      //res.writeHead(200, headers);
+      //res.end(headers);
+    },
+    post: function (req, res) {
+      console.log("post users models");
+      //res.writeHead(200, headers);
+      //res.end(headers);
+    }
   }
 };
 
